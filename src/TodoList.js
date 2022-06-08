1569ecffe8c5f3c56afd967053f73ef491b1a573
Colin F. Han <h.fly.colin@gmail.com>
2022-06-07 23:50:45 -0500

    feat: proptypes

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import TodoItem from "./TodoItem";
+import "./style.css"
 
 class TodoList extends Component {
 
@@ -24,22 +25,25 @@ class TodoList extends Component {
 
   handleBtnClick() {
     if (this.state.inputValue === 0 || !!this.state.inputValue)
-      this.setState({
-        list: [...this.state.list, this.state.inputValue],
+      this.setState((prevState) => ({
+        list: [...prevState.list, prevState.inputValue],
         inputValue: ''
-      })
+      }))
   }
 
   handleInputChange(e) {
-    this.setState({
-      inputValue: e.target.value
-    })
+		const value = e.target.value
+    this.setState(() => ({
+      inputValue: value
+    }))
   }
 
   handleDelete(index) {
-    const list = [...this.state.list]
-    list.splice(index, 1)
-    this.setState({list})
+    this.setState((prevState) => {
+			const list = [...prevState.list]
+    	list.splice(index, 1)
+			return {list}
+		})
   }
 
   getTodoItems() {
@@ -61,7 +65,8 @@ class TodoList extends Component {
     return (
       <div>
         <div>
-          <input value={this.state.inputValue} onChange={this.handleInputChange} onKeyDown={this.manageEnter} />
+          <label htmlFor="task">Task name</label>
+          <input id="task" className="input-area" value={this.state.inputValue} onChange={this.handleInputChange} onKeyDown={this.manageEnter} />
           <button onClick={this.handleBtnClick}>add</button>
         </div>
         <ul>{this.getTodoItems()}</ul>
